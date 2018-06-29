d61ce98d94226ecc9d653ae9e12ff0269c50115b
Daniel <danielsantospatrocinio@gmail.com>
2018-06-29 19:50:26 -0300

    versão1

---
+++ src/mexendocomgit/MexendoComGit.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mexendocomgit;
+
+/**
+ *
+ * @author danie
+ */
+public class MexendoComGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+            System.out.println("olá mundo");
+            System.out.println("eeeeei");
+        
+        
+    }
+    
+}
